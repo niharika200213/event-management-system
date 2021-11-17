a8349d8027863dc05ff704f2f33878c6b228ad9e
niharika rastogi <rastoginiha2002@gmail.com>
2021-11-17 23:43:57 +0530

    images

--- middleware/helpers.js
+++ middleware/helpers.js
@@ -1,4 +1,5 @@
 const multer = require('multer');
+const path=require('path');
 
 const imageFilter = function(req, file, cb) {
     if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG)$/)) {
@@ -10,7 +11,7 @@ const imageFilter = function(req, file, cb) {
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'images');
+        cb(null, path.resolve(__dirname, 'images'));
       },
     filename: function (req, file, cb) {
         cb(null, String(Date.now()+file.originalname.replace(' ','-')));
