5b8d6a0323abfd9a1c83fa635dedb609370e506e
niharika rastogi <rastoginiha2002@gmail.com>
2021-11-27 18:47:08 +0530

    creator

--- models/otp.js
+++ models/otp.js
@@ -12,7 +12,7 @@ const otpSchema = new Schema({
       type: String,
       required: true
   },
-  createdAt: {type: Date, default: Date.now(), expires: '10m'}
+  createdAt: {type: Date, default: Date.now, expires: "5m"}
 });
 
 module.exports = mongoose.model('OTP', otpSchema);
