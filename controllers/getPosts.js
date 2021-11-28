536fd808b6d81a909d483678f4e2b6cdea90ee84
niharika rastogi <rastoginiha2002@gmail.com>
2021-11-28 11:15:57 +0530

    creator

--- controllers/getPosts.js
+++ controllers/getPosts.js
@@ -66,7 +66,9 @@ exports.getPost = async (req,res,next) => {
             return res.status(401).send('the event does not exists');
         const user = await User.findById(post.creator);
         if(!user.isCreator){
-            if(user._id!==curUserId)
+            if(user._id===curUserId)
+                return res.status(200).send({post:post});
+            else 
                 return res.status(401).send('not a verified post');
         }
         else if(curUserId===null)
