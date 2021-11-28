3894c060bbf5b001bd522e5e955de5b18dd54bf4
niharika rastogi <rastoginiha2002@gmail.com>
2021-11-28 09:22:50 +0530

    creator

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({extended:true}));
 
 app.use(cors({
   origin: "*",
-  methods: ['GET','POST','PUT','DELETE']
+  methods: ['GET','POST','PUT','DELETE', 'PATCH']
 }));
 app.use('/auth',authRoutes);
 app.use('/post',postRoutes);
