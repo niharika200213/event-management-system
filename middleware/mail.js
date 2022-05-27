0c459ea6a6ed05b7026c8ccb0968283f0d676af6
niharika rastogi <rastoginiha2002@gmail.com>
2022-05-27 21:52:45 +0530

    aki key change

--- middleware/mail.js
+++ middleware/mail.js
@@ -1,35 +1,13 @@
-const mailjet = require('node-mailjet')
-    .connect(process.env.MJ_APIKEY_PUBLIC, process.env.MJ_APIKEY_PRIVATE)
+const nodemailer=require('nodemailer');
+const sendgridTransport=require('nodemailer-sendgrid-transport');
+
+const transporter=nodemailer.createTransport(sendgridTransport({
+    auth:{api_key: process.env.API_KEY}
+}));
 
 module.exports = (email, subject, html) => {
-    const request = mailjet
-        .post("send", { 'version': 'v3.1' })
-        .request({
-            "Messages": [
-                {
-                    "From": {
-                        "Email": "eventooze@gmail.com",
-                        "Name": "Eventooze"
-                    },
-                    "To": [
-                        {
-                            "Email": email
-                        }
-                    ],
-                    "TemplateID": 3964147,
-                    "TemplateLanguage": true,
-                    "Subject": subject,
-                    "Variables": {
-                        "html": html
-                    }
-                }
-            ]
-        })
-    request
-        .then((result) => {
-            console.log(result.body)
-        })
-        .catch((err) => {
-            console.log(err.statusCode)
-        })
+    transporter.sendMail({
+        to: email, from: 'learnatstuista@gmail.com',
+        subject: subject, html: html
+      });
 }
